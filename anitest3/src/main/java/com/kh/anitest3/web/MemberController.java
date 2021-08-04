297a0d1649f46cef00233917a9b0e235e020e3b6
hjlee0820 <umtt0820@gmail.com>
2021-08-05 00:01:46 +0900

    회원유형에 따라 회원가입페이지 다르게 보이기 완료
    커밋

--- anitest3/src/main/java/com/kh/anitest3/web/MemberController.java
+++ anitest3/src/main/java/com/kh/anitest3/web/MemberController.java
@@ -1,6 +1,7 @@
 package com.kh.anitest3.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,15 +24,29 @@ public class MemberController {
 	private final MemberSVC memberSVC;
 	
 	/**
+	 * 회원가입유형선택
+	 */
+	@GetMapping("/join")
+	public String joinSelectMtype() {
+		
+		return "/member/joinSelectMtype";
+	}
+	/**
 	 * 회원가입폼
 	 * @return
 	 */
-	@GetMapping("/join")
-	public String joinForm() {
+	@GetMapping("/join/{mtype}")
+	public String joinForm(
+			@PathVariable("mtype") String mtype, Model model) {
 		
 		//회원유형 선택화면에서 회원유형값 받아서 조인폼으로 넘겨야됨.
 		//조인폼에서 받은 회원유형 값으로 보여줄 항목, 안보여줄 항목 선택되도록
 		
+		if(mtype.equals("일반"))	mtype="N";
+		if(mtype.equals("전문가"))	mtype="P";
+		if(mtype.equals("사업자"))	mtype="B";
+		
+		model.addAttribute("mtype", mtype);
 		return "/member/joinForm";
 	}
 	
